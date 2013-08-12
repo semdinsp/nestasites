cc83f75fb340e058afc5d8de04f1d71ea1b7e9e5
semdinsp <scott.sproule@ficonab.com>
2013-08-12 16:54:33 +0800

    now estorm has test page

---
+++ estormtech.com/test_app.rb
@@ -0,0 +1,47 @@
+require 'test/unit'
+require 'rubygems'
+gem 'rack-test'
+require 'rack/test'
+require 'nesta/env'
+Nesta::Env.root = ::File.expand_path('.', ::File.dirname(__FILE__))
+
+require 'nesta/app'
+
+ENV['RACK_ENV'] = 'test'
+
+class EstormTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Nesta::App
+  end
+
+  def test_first_pageworks
+    get '/'
+    #puts last_response.methods
+    assert last_response.ok?
+    assert last_response.body.include? 'Estorm Technologies Pte. Ltd.'
+  end
+
+  def test_key_pages
+     pagelist=["contact","support","about","clients","products","products/crm", "/"]
+     pagelist.each { |page| 
+          get page
+          assert last_response.ok?, "#{page} not found"
+          #assert_equal last_response.body.includes? 'Ficonab Pte. Ltd.'
+            }
+   end
+   def test_products_pages
+      pagelist=["crm","dms","scratch","tms","sms"]
+      pagelist.each { |page| 
+           get "products/#{page}"
+           assert last_response.ok?, "products/#{page} not found"
+           #assert_equal last_response.body.includes? 'Ficonab Pte. Ltd.'
+             }
+    end
+   
+  #def test_it_says_hello_to_a_person
+  #  get '/', :name => 'Simon'
+  #  assert last_response.body.include?('Simon')
+  # end
+end
